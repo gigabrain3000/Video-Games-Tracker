c01efda45c98838a6344eebcd11b879f1419911c
gigabrain3000 <vladislav.ivanchikov15@gmail.com>
2025-10-16 19:43:54 +0300

    fix: build project for deploy

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,14 +7,15 @@ export default function Header(): ReactElement {
   return (
     <header className="display: flex items-center justify-between">
       <h1 className="">Tracker</h1>
-      <nav className="display: flex gap-3">
+      <nav className="display: flex gap-3 items-center">
+        <div className="display: flex items-center cursor-pointer">
+          <h3>Guest</h3>
+          <ChevronDown />
+        </div>
+        <a href="#" className="no-underline cursor-pointer">Games</a>
         <div className="display: flex gap-1.5">
           <Input placeholder="Search games" />
           <Button variant={"outline"}>Search</Button>
-        </div>
-        <div className="display: flex items-center">
-          <h3>Guest</h3>
-          <ChevronDown />
         </div>
         <Button variant={"outline"}><Plus />Add a Game</Button>
       </nav>
